ddfc24dabaa744801da0c7c19c2a56a89e3d0a93
rajaathota72-fc <57040106+rajaathota72-fc@users.noreply.github.com>
2020-08-22 11:05:04 +0530

    Add files via upload

---
+++ LMSworkingcode/mainrevised.py
@@ -0,0 +1,106 @@
+import os
+import cv2
+import numpy as np
+from keras.models import model_from_json
+from keras.preprocessing import image
+#load model
+#model = model_from_json(open("fer.json", "r").read())
+#load weights
+#model.load_weights('fer.h5')
+from flask import Flask, render_template, Response
+from camera import VideoCamera
+
+app = Flask(__name__)
+import cv2
+facec=cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+ds_factor=0.6
+font = cv2.FONT_HERSHEY_SIMPLEX
+from tensorflow.keras.models import model_from_json
+from tensorflow.python.keras.backend import set_session
+import numpy as np
+
+import tensorflow as tf
+
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.15
+session = tf.compat.v1.Session(config=config)
+set_session(session)
+
+
+class FacialExpressionModel(object):
+
+    EMOTIONS_LIST = ["Angry", "Disgust",
+                     "Fear", "Happy",
+                     "Neutral", "Sad",
+                     "Surprise"]
+
+    def __init__(self, model_json_file, model_weights_file):
+        # load model from JSON file
+        with open(model_json_file, "r") as json_file:
+            loaded_model_json = json_file.read()
+            self.loaded_model = model_from_json(loaded_model_json)
+
+        # load weights into the new model
+        self.loaded_model.load_weights(model_weights_file)
+        #self.loaded_model.compile()
+        #self.loaded_model._make_predict_function()
+
+    def predict_emotion(self, img):
+        global session
+        set_session(session)
+        self.preds = self.loaded_model.predict(img)
+        return FacialExpressionModel.EMOTIONS_LIST[np.argmax(self.preds)]
+model = FacialExpressionModel("fer.json", "fer.h5")
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+    
+    def __del__(self):
+        self.video.release()
+    
+    def get_frame(self):
+        _, fr = self.video.read()
+        gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+        faces = facec.detectMultiScale(gray_fr, 1.3, 5)
+
+        for (x, y, w, h) in faces:
+            fc = gray_fr[y:y+h, x:x+w]
+
+            roi = cv2.resize(fc, (48, 48))
+            pred = model.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
+
+            cv2.putText(fr, pred, (x, y), font, 1, (255, 255, 0), 2)
+            cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
+
+        _, jpeg = cv2.imencode('.jpg', fr)
+        return jpeg.tobytes()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+from werkzeug.debug import DebuggedApplication
+
+def create_app():
+    # Insert whatever else you do in your Flask app factory.
+
+    if app.debug:
+        app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
+
+    return app
+
+if __name__ == '__main__':
+    app.run()
